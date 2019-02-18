2f73517b31cec39c4b776d6019a0122146d4b48d
Fabian Scherer <fabian-scherer98@outlook.de>
2019-02-18 19:07:31 +0100

    try statement with resources in HTTPServer.java

--- src/dv201/httpserver/HTTPServer.java
+++ src/dv201/httpserver/HTTPServer.java
@@ -6,57 +6,40 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-
-
 public class HTTPServer {
 
     private static final String ARGS_USAGE = "usage: MyPort\n";
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         int myPort;
 
         if (args.length != 1) {
-           FatalError(ARGS_USAGE);
+            FatalError(ARGS_USAGE);
             return;
         }
         String _myPortStr = args[0];
 
         // check the port number
         myPort = HTTPServerLib.ParsePort(_myPortStr);
-        if(myPort <= 0){
+        if (myPort <= 0) {
             FatalError(ARGS_USAGE);
             return;
         }
 
-        // create the server socket
-        ServerSocket serverSocket;
-        try {
-            serverSocket = new ServerSocket(myPort);
-        } catch (Exception e) {
-            FatalError("Exception while creating the socket");
-            return;
-        }
-
-        //accept a incoming connection and run the async echo reply
-        int requestCount = 0;
-        ExecutorService ex = Executors.newCachedThreadPool();
-        try {
+        try (ServerSocket serverSocket = new ServerSocket(myPort)) {
+            int requestCount = 0;
             while (true) {
                 Socket socket = serverSocket.accept();
                 System.out.println("Accepting New Request: " + requestCount++);
                 new Thread(new InboundRequest(socket)).start();
-//                CompletableFuture.runAsync(() -> AcceptIncomingConnection(socket), ex);
             }
         } catch (Exception e) {
-            System.err.println("Exception while waiting for connections" + e.toString());
-            try {
-                serverSocket.close();
-            } catch (Exception f) {
-                System.err.println("Execption closing socket" + e.toString());
-            }
+            FatalError("Error creating or accepting server socket." + e.getMessage());
         }
 
     }
-    private static void FatalError(String errorMsg){
+
+    private static void FatalError(String errorMsg) {
         System.err.println(errorMsg);
         System.exit(1);
     }
